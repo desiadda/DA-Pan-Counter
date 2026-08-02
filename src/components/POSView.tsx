ebcddec63717a37a0a93f4827f3d558d648fa423
Jigar Patel <desiaddathailand@gmail.com>
2026-08-02 19:20:37 +0700

    fix: Import DEFAULT_PACK_SIZE and DEFAULT_DISCOUNT_REASONS in POSView.tsx

--- src/components/POSView.tsx
+++ src/components/POSView.tsx
@@ -16,7 +16,7 @@ import DashboardWidgets from "./DashboardWidgets";
 import ShortcutsModal from "./ShortcutsModal";
 import { useLangStore } from "../stores/langStore";
 import { useT } from "../lang/translations";
-import { DEFAULT_VAT_RATE } from "../constants";
+import { DEFAULT_VAT_RATE, DEFAULT_PACK_SIZE, DEFAULT_DISCOUNT_REASONS } from "../constants";
 
 export default function POSView({ user }) {
   const lang = useLangStore((s) => s.lang);
